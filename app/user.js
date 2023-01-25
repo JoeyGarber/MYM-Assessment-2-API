cdf73d6094ae92b86056f5303b1266f424c315ee
joeygarber <joseph.z.garber@gmail.com>
2023-01-25 13:09:18 -0500

    Added OAuth routes and passport middleware
    Setting up DB to persist credentials next

--- app/user.js
+++ app/user.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
+  id: {
+    type: String
+  },
+  name: {
+    type: String
+  },
   email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  hashedPassword: {
-    type: String,
-    required: true
-  },
-  token: String
+    type: String
+  }
 }, {
   timestamps: true,
   toObject: {
--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 const express = require('express')
+const passport = require('passport')
+const session = require('express-session')
+const auth = require('./lib/auth')
 
 const app = express()
-const PORT = 8080
 
+app.use(session({
+  secret: 'thisismysecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+const PORT = 8080
+app.use(auth)
+app.use(passport.session())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({ message: "You are not logged in"})
+})
+
+app.get('/google',
+  passport.authenticate('google', {
+    scope: ['email', 'profile']
+  }))
+
+app.get('/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure'
+  }),
+  function (req, res) {
+    console.log(req.user)
+    res.redirect('/success')
+  })
+  
+  app.get('/failure', (req, res) => {
+    res.send("Failed.")
+  })
+
+  app.get('/success', (req, res) => {
+    res.send(req.user)
+  })
 
 app.listen(PORT, (error) => {
   if (!error) {
